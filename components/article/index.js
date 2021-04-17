67d07b067f9cdc6c490c6bc02b523998fffd4b68
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2021-04-17 09:56:12 +0200

    updated design of article and removed carrousel

--- components/article/index.js
+++ components/article/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
+import ContainerFluid from '../layouts/ContainerFluid';
 import Container from '../layouts/Container';
 import styles from './Article.module.scss';
 
 const Article = ({ index, topArticle }) => {
 	return (
-		<div key={index} className='mb-96 w-screen'>
-			<img
-				src={topArticle.multimedia[0].url}
-				alt=''
-				className={`w-full object-cover ${styles.carouselImg}`}
-			/>
-			<Container>
-				<div className='w-8/12 m-auto'>
-					<h2>{topArticle.title}</h2>
-					<p>{topArticle.abstract}</p>
+		<div key={index} className='mb-16 w-screen'>
+			<ContainerFluid>
+				<div className='mb-4'>
+					<img src={topArticle.multimedia[0].url} alt='' className={`w-full`} />
 				</div>
-			</Container>
+				<Container>
+					<h2 className='mb-2'>{topArticle.title}</h2>
+					<p>{topArticle.abstract}</p>
+				</Container>
+			</ContainerFluid>
+
 			{/* <button onClick={() => like(article)}>Like</button> */}
 		</div>
 	);
